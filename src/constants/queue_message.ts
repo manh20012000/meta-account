a160df12f14414116bbcb147a296633bb548beec
Lê Mạnh <121529297+manh20012000@users.noreply.github.com>
2025-10-15 11:51:04 +0700

    update route

--- src/constants/queue_message.ts
+++
@@ -1,5 +0,0 @@
-export const ROUTING_KEYS = {
-    ACCOUNT_USER_CREATED: 'account.user.created',
-    ACCOUNT_USER_UPDATED: 'account.user.updated',
-    ACCOUNT_USER_DEACTIVATED: 'account.user.deactivated',
-  } ;
